8f5630f03b1562ea7d3cf06c5b483fb4d7abfea2
Saivenkat1903 <86838056+Saivenkat1903@users.noreply.github.com>
2023-01-24 01:16:06 +0530

    added comments

--- Session_1/add.java
+++ Session_1/add.java
@@ -1,10 +1,10 @@
-public class add {
-    public static void main(String[] args) {
-        int a=5;
-        int b=7;
+public class add { //defines a class named add
+    public static void main(String[] args) { //again the main method that Java detects
+        int a=5;        //initiates and declares an integer variable a with the value 5
+        int b=7;         //initiates and declares an integer variable b with the value 7
         
-        int c = a+b;
-        System.out.println(c);
+        int c = a+b;             //initiates and declares an integer variable c with the value a+b 
+        System.out.println(c);   //Prints out the value of the variable
         System.out.println(a+b);
     }
 }
