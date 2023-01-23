3c68cc21a21b0722b8d56f3645bf96a7ef8f745f
Saivenkat1903 <86838056+Saivenkat1903@users.noreply.github.com>
2023-01-24 01:14:02 +0530

    added Comments

--- Session_1/hello.java
+++ Session_1/hello.java
@@ -1,5 +1,5 @@
-public class hello{
-    public static void main(String[] args){
-        System.out.println("hello world");
+public class hello{     //Defines a class named hello
+    public static void main(String[] args){ //the main method that Java looks for
+        System.out.println("hello world");  //syntax to print out to console
     }
 }
