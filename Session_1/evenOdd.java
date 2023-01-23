c59817ddc8518c4f7a2d28daf51358b4003b6b77
Saivenkat1903 <86838056+Saivenkat1903@users.noreply.github.com>
2023-01-24 01:19:15 +0530

    added comments

--- Session_1/evenOdd.java
+++ Session_1/evenOdd.java
@@ -1,14 +1,14 @@
-public class evenOdd {
+public class evenOdd { 
     public static void main(String[] args) {
         int num = 421;
 
-        int condition = num%2;
-        System.out.println(condition);
+        int condition = num%2;          //modulo function in Java is denoted by % , it gives the remainder when divieded by something
+        System.out.println(condition); 
 
-        if(num%2 == 0){
-            System.out.println("Number is even");
+        if(num%2 == 0){                                 //== is the syntax to check if 2 numbers are equal to each other
+            System.out.println("Number is even");       //This line of code only runs if above condition evaluates to True
         }else{
-            System.out.println("Number is odd");
+            System.out.println("Number is odd");       //This line of code runs if the above "if" statement evalutates to false 
         }
     }
 }
